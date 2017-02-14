2458baf7fc05ad2663307b370f6bcdfc012aa533
unknown <arekpl28@wp.pl>
2017-02-14 21:04:00 +0100

    Added count sort

---
+++ src/Algorytmu/sort/CountSort.java
@@ -0,0 +1,33 @@
+package Algorytmu.sort;
+
+import java.util.Arrays;
+
+public class CountSort {
+
+    public static void main(String[] args) {
+        CountSort countSort = new CountSort();
+        int[] array = {-2, -5, 3, 4, 1, 0, 0, 0, -3, 5, 3};
+        System.out.println(Arrays.toString(countSort.ascSort(array)));
+
+
+    }
+
+    public int[] ascSort(int[] array) {
+        int max = SortUtils.max(array);
+        int min = SortUtils.min(array);
+        int range = max - min + 1;
+        int[] rangeArray = new int[range];
+        for (int i = 0; i < array.length; i++) {
+            rangeArray[array[i] - min]++;
+        }
+        int index = 0;
+        for (int i = 0; i < rangeArray.length; i++) {
+            for (int j = 0; j < rangeArray[i]; j++) {
+                array[index] = i + min;
+                index++;
+            }
+        }
+        return array;
+    }
+
+}
--- src/Algorytmu/sort/SortUtils.java
+++ src/Algorytmu/sort/SortUtils.java
@@ -3,9 +3,29 @@ package Algorytmu.sort;
 public class SortUtils {
 
 
-    public static void swap(int [] array, int index1, int index2){
+    public static void swap(int[] array, int index1, int index2) {
         int tmp = array[index1];
         array[index1] = array[index2];
         array[index2] = tmp;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 }
