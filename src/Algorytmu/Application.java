7dfaa8fa7934bacd43cdad67a51ef6dc59357948
unknown <arekpl28@wp.pl>
2017-02-15 19:43:50 +0100

    Added MyList classes

--- src/Algorytmu/Application.java
+++ src/Algorytmu/Application.java
@@ -1,8 +1,25 @@
 package Algorytmu;
 
 
+import Algorytmu.structures.MyList;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        MyList myList= new MyList();
+        myList.add(2);
+        myList.add(1);
+        myList.add(1);
+        myList.add(-2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(0);
+        myList.add(0);
+        myList.add(0);
+        myList.add(7);
+        myList.add(5);
+        myList.add(5);
+        myList.add(5);
+        myList.add(5);
+        System.out.println(myList.get(12));
     }
 }
