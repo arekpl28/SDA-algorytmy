8ee60cd8f9df8e0e675c14f740550ed0c6daa627
unknown <arekpl28@wp.pl>
2017-02-18 10:53:51 +0100

    Finished linked list

--- src/test/algorytmy/structures/MyLinkedListTest.java
+++ src/test/algorytmy/structures/MyLinkedListTest.java
@@ -2,6 +2,7 @@ package test.algorytmy.structures;
 
 
 import algorytm.structures.MyLinkedList;
+import algorytm.structures.MyList;
 import algorytm.structures.MyListInterface;
 
 public class MyLinkedListTest {
@@ -17,7 +18,38 @@ public class MyLinkedListTest {
         System.out.println(myLinkedList);
         myLinkedList.delete(2);
         System.out.println(myLinkedList);
+        myLinkedList.put(2, 333);
+        System.out.println(myLinkedList);
+        MyListInterface someList = new MyList();
+        someList.add(2);
+        someList.add(3);
+        someList.add(-1);
+        System.out.println();
 
+        System.out.println("Before and after adding some list");
+        System.out.println(myLinkedList);
+        myLinkedList.addAll(someList);
+        System.out.println(myLinkedList);
+        System.out.println();
+
+        MyListInterface clonedList = myLinkedList.clone();
+        clonedList.delete(2);
+        System.out.println("After removing value from cloned list");
+        System.out.println(myLinkedList);
+        System.out.println(clonedList);
+        System.out.println();
+
+        System.out.println("Before and adding element on index 2");
+        System.out.println(myLinkedList);
+        myLinkedList.add(2, 555);
+        System.out.println(myLinkedList);
+        myLinkedList.add(0, 444);
+        System.out.println(myLinkedList);
+
+        System.out.println("Before and after adding all into");
+        System.out.println(myLinkedList);
+        myLinkedList.addAll(2, someList);
+        System.out.println(myLinkedList);
 
     }
 }
